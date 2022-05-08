364490b77a27508e1fb12d8a4a779077fa8e2947
snoopy2080 <49978401+snoopy2080@users.noreply.github.com>
2022-05-08 19:56:42 +0900

    Working on 02-Fundamentals-Part-2

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -125,17 +125,28 @@ percentage values
 
 /*
 LECTURE: Basic Array Operations (Methods)
-1. Create an array containing all the neighbouring countries of a country of your
-choice. Choose a country which has at least 2 or 3 neighbours. Store the array
-into a variable called 'neighbours'
-2. At some point, a new country called 'Utopia' is created in the neighbourhood of
-your selected country. So add it to the end of the 'neighbours' array
+1. Create an array containing all the neighboring countries of a country of your
+choice. Choose a country which has at least 2 or 3 neighbors. Store the array
+into a variable called 'neighbors'
+2. At some point, a new country called 'Utopia' is created in the neighborhood of
+your selected country. So add it to the end of the 'neighbors' array
 3. Unfortunately, after some time, the new country is dissolved. So remove it from
 the end of the array
-4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+4. If the 'neighbors' array does not include the country ‘Germany’, log to the
 console: 'Probably not a central European country :D'
-5. Change the name of one of your neighbouring countries. To do that, find the
-index of the country in the 'neighbours' array, and then use that index to
+5. Change the name of one of your neighboring countries. To do that, find the
+index of the country in the 'neighbors' array, and then use that index to
 change the array at that index position. For example, you can search for
 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
 */
+
+// const neighbors = new Array("Japan", "China", "North Korea", "Taiwan");
+// neighbors.push("Utopia");
+// console.log(neighbors);
+// neighbors.pop();
+// console.log(neighbors);
+// if (neighbors.includes("Germany")) {
+//   console.log("Probably a central European country :D");
+// } else console.log("Probably not a central European country :D");
+// neighbors[neighbors.indexOf("North Korea")] = "Sweden";
+// console.log(neighbors);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -140,19 +140,35 @@ const ages = [
   calcAge(years[years.length - 1]),
 ];
 console.log(ages);
-*/
 
 const friends = ["Michael", "Steven", "Peter"];
 
 // Add elements
-const newLength = friends.push("Jay");
+const newLength = friends.push("Jay"); // Last
 console.log(friends);
 console.log(newLength);
 
-friends.unshift("John");
+friends.unshift("John"); // First
 console.log(friends);
 
 // Remove elements
-friends.pop();
-friends.pop();
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
 console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23)); // No type coercion
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
+*/
